3713942fbcd68c22c27a2715ad02721070c3b847
Arif <arifajif@gmail.com>
2022-06-01 22:09:19 +0700

    tugas 4

---
+++ tugas4.js
@@ -0,0 +1,20 @@
+var A = 176;
+var B = 163;
+var C = 172;
+
+if (A>B) {
+    if(A>C) {
+        console.log("A adalah yang tertinggi")
+    } 
+    else {
+        console.log("A adalah yang tertinggi kedua")
+    }
+}
+else {
+    if (A<C) {
+    console.log("A adalah yang terpendek")
+}
+else {
+    console.log ("A adalh yang terpendek kedua")
+}
+}
